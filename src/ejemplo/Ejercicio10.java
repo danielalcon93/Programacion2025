81be93982678c3df77963d1a9e827617f4d297df
Alcon93 <danielfernandezalcon93@gmail.com>
2025-10-06 17:49:34 +0200

    Add files via upload

---
+++ src/ejemplo/Ejercicio10.java
@@ -0,0 +1,14 @@
+package ejemplo;
+
+public class Ejercicio10 {
+
+    static void main(String[] args) {
+
+        int num=5;
+        num += num - 1 * 4 + 1;
+        System.out.println(num);
+        num=4;
+        num %= 7 * num % 3 * 5;
+        System.out.println(num);
+    }
+}
