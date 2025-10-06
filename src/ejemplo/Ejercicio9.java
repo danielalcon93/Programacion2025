a9c46ed7554654ca90d7296db4f7ef4b450e8adf
Alcon93 <danielfernandezalcon93@gmail.com>
2025-10-06 17:46:56 +0200

    Add files via upload

---
+++ src/ejemplo/Ejercicio9.java
@@ -0,0 +1,12 @@
+package ejemplo;
+
+public class Ejercicio9 {
+
+    static void main(String[] args) {
+
+        int numero=2;
+        int cuadrado;
+        cuadrado=numero * numero;
+            System.out.println("EL CUADRADO DE "+numero+" ES: " + cuadrado);
+        }
+    }
