9d3ece2d8b74f3433c1421ef7f75c1ef07dae3a0
Alcon93 <danielfernandezalcon93@gmail.com>
2025-10-06 17:42:33 +0200

    Add files via upload

---
+++ src/ejemplo/Ejercicio7.java
@@ -0,0 +1,15 @@
+package ejemplo;
+
+public class Ejercicio7 {
+
+    static void main(String[] args) {
+
+        int n1=50;
+        int n2=30;
+        int suma;
+        suma=n1+n2;
+        System.out.println("LA SUMA ES: " + suma);
+
+
+    }
+}
---
+++ src/ejemplo/Ejercicio8.java
@@ -0,0 +1,17 @@
+package ejemplo;
+
+public class Ejercicio8 {
+
+    public static void main(String[] args) {
+
+        int n1=50;
+        int n2=30;
+        int n3=20;
+        int suma;
+            suma=n1+n2;
+            System.out.println("LA SUMA ES: " + suma);
+            suma=suma+n3;
+            System.out.println(suma);
+        }
+    }
+
