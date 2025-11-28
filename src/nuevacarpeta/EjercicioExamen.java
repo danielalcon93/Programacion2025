140ba49237e8eeae23e2b4783ec2d32bb26b1b48
Alcon93 <danielfernandezalcon93@gmail.com>
2025-11-28 19:28:59 +0100

    Add files via upload

---
+++ src/nuevacarpeta/EjercicioExamen.java
@@ -0,0 +1,20 @@
+package ExamenAna;
+
+public class EjercicioExamen {
+
+    static void main(String[] args) {
+
+
+            // 1. Variable para almacenar el sueldo
+            double sueldo = 1500.0; // valor inicial
+
+            // 2. Variable para almacenar los gastos
+            double gastos = 900.0; // valor inicial
+
+            // 3. Variable para almacenar el ahorro (sueldo - gastos)
+            double ahorro = sueldo - gastos;
+
+            // Mostrar el resultado
+            System.out.println("El ahorro es: " + ahorro);
+    }
+}
