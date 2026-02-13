14758c7226b517e30324d30eb80e66cf4184053d
Alcon93 <danielfernandezalcon93@gmail.com>
2026-02-13 11:48:58 +0100

    Add files via upload

--- src/Juego/Partida/nuevaPartida.java
+++ src/Juego/Partida/nuevaPartida.java
@@ -1,7 +1,9 @@
 package Juego.Partida;
 
+import Juego.Jugador.Arma;
 import Juego.Jugador.Jugador;
 import Juego.Monstruo.Monstruo;
+import Juego.Monstruo.MonstruoFinalNivel;
 
 import java.util.ArrayList;
 
@@ -14,10 +16,48 @@ public class nuevaPartida {
     public nuevaPartida(Jugador jugador) {
         this.jugador = jugador;
         this.monstruos = new ArrayList<>();
-        iniciarPartida();
     }
 
+
     //Getter
+    public Jugador getJugador() {
+        return jugador;
+    }
+
+    public void setJugador(Jugador jugador) {
+        this.jugador = jugador;
+    }
+
+    public ArrayList<Monstruo> getMonstruos() {
+        return monstruos;
+    }
+
+    public void setMonstruos(ArrayList<Monstruo> monstruos) {
+        this.monstruos = monstruos;
+    }
+
+    //toString
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("nuevaPartida{");
+        sb.append("jugador=").append(jugador);
+        sb.append(", monstruos=").append(monstruos);
+        sb.append('}');
+        return sb.toString();
+    }
 
 
+    //Metodos
+
+
+    public void turnoEnemigos() {
+        if (monstruos.size() == 0)
+            return;
+        Monstruo enemigo = monstruos.get(0);
+
+        enemigo.golpear(jugador);
+        if (jugador.getSalud() <= 0) {
+            return;
+        }
+    }
 }
